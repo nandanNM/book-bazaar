fb1bf461f54a7d81d0d0368a42485b56686b31ae
CoderNandan <nandanindia1947@gmail.com>
2025-06-17 22:40:02 +0530

    Add book model with schema and validation rules

---
+++ src/models/book.model.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const bookSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    authors: [String],
+    publisher: String,
+    publishedDate: {
+      type: Date,
+      default: Date.now,
+    },
+    description: String,
+    isbn10: String,
+    isbn13: { type: String, unique: true },
+    imageLinks: {
+      smallThumbnail: String,
+      thumbnail: String,
+    },
+    language: { type: String, default: "en" },
+    price: {
+      type: Number,
+      required: true,
+      min: 50,
+    },
+    stock: { type: Number, default: 0 },
+    category: {
+      type: String,
+      enum: [
+        "FICTION",
+        "NON_FICTION",
+        "COMIC",
+        "EDUCATION",
+        "SCIENCE",
+        "OTHER",
+      ],
+      default: "OTHER",
+      uppercase: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Book = mongoose.model("Book", bookSchema);
