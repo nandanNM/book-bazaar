b69ba700b3e7dfe2177a97457b1995605bf24118
CoderNandan <nandanindia1947@gmail.com>
2025-06-17 19:38:43 +0530

    Add user model with schema, password hashing, and JSON transformation

---
+++ src/models/user.model.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, trim: true },
+    email: { type: String, required: true, trim: true, unique: true },
+    password: { type: String, required: true, trim: true, select: false },
+    role: {
+      type: String,
+      enum: ["ADMIN", "USER"],
+      default: "USER",
+      uppercase: true,
+    },
+    isVerified: { type: Boolean, default: false },
+    isDeleted: { type: Boolean, default: false, select: false },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// Remove sensitive fields from JSON output
+userSchema.methods.toJSON = function () {
+  const obj = this.toObject();
+  delete obj.password;
+  delete obj.__v;
+  return obj;
+};
+
+const User = mongoose.model("User", userSchema);
+export default User;
