4290ff59109a52fecdc757d538f93766d1b3d904
CoderNandan <nandanindia1947@gmail.com>
2025-06-18 17:20:43 +0530

    Add review model with schema and unique index for book and user

---
+++ src/models/review.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const reviewSchema = new mongoose.Schema(
+  {
+    bookId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Book",
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    review: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
+  },
+  { timestamps: true }
+);
+reviewSchema.index({ bookId: 1, userId: 1 }, { unique: true });
+export const Review = mongoose.model("Review", reviewSchema);
